1b9e1bbca226950b74ee9874713843c6d9881027
David Urbano <david.urbano@uao.edu.co>
2023-12-20 08:51:29 -0500

    fix: logout link in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://sanurb.github.io',
-  base: '/aavance-portal-recargas'
+	integrations: [tailwind()],
+	site: "https://sanurb.github.io",
+	base: "/aavance-portal-recargas",
 });
